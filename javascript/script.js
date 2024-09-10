263253996c56770239a9bf8311ad0b391eb7d39e
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-10 14:47:55 +0800

    feat: define the changeTheme func
    Adds and removes the class: dark-theme to the body
    whenever the theme icon is clicked.

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,15 @@
 const icon = document.querySelector("#icon");
 const logo = document.querySelector(".logo");
 
-
+function changeTheme() {
+  document.body.classList.toggle("dark-theme");
+  if(document.body.classList.contains("dark-theme")) {
+    icon.src = "./images/sun.png";
+    logo.src = "./images/kyle_logo_dark.png";
+  } else {
+    icon.src = "./images/moon.png";
+    logo.src = "./images/kyle_logo.png";
+  }
+}
 
 icon.addEventListener("click", changeTheme);
